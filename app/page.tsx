ab3f15b63d0d92e299cc79acd3bdb7fe67ce3422
Dhrish Parekh <parekhdhrish.pg@gmail.com>
2024-02-14 18:42:23 +0530

    made config for adding embeddings to Supabase Vector

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import { Poppins } from "next/font/google";
-
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import { LoginButton } from "@/components/auth/login-button";
@@ -11,25 +10,10 @@ const font = Poppins({
 
 export default function Home() {
   return (
-    <main className="flex h-full flex-col items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800">
-      <div className="space-y-6 text-center">
-        <h1 className={cn(
-          "text-6xl font-semibold text-white drop-shadow-md",
-          font.className,
-        )}>
-          🔐 Auth
-        </h1>
-        <p className="text-white text-lg">
-          A simple authentication service
-        </p>
-        <div>
-          <LoginButton  asChild>
-            <Button variant="secondary" size="lg">
-              Sign in
-            </Button>
-          </LoginButton>
-        </div>
-      </div>
-    </main>
+    <>
+    <div>
+      <h2>Hi from ap-south-1</h2> 
+    </div>
+    </>
   )
 }
---
+++ scripts/embeddings.ts
@@ -0,0 +1,124 @@
+import { OpenAI } from "openai";
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+import { PDFLoader } from "langchain/document_loaders/fs/pdf";
+import dotenv from "dotenv";
+import { encode } from "gpt-tokenizer/model/gpt-3.5-turbo-0301";
+import { createClient } from "@supabase/supabase-js";
+dotenv.config();
+
+const createEmbedding = async (
+  title: string,
+  author: string,
+  content: string,
+  i: number
+) => {
+  const supabase = createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.SUPABASE_SERVICE_ROLE_KEY!
+  );
+  const JSONContent = {
+    title: title,
+    author: author,
+    content: content,
+  };
+  if (encode(JSON.stringify(JSONContent)).length > 8000) {
+    const midpoint = Math.floor(JSON.stringify(JSONContent).length / 2);
+    const firstHalf = JSON.stringify(JSONContent).slice(0, midpoint);
+    console.log(encode(JSON.stringify(firstHalf)).length);
+    const secondHalf =JSON.stringify(JSONContent).slice(midpoint);
+    console.log(encode(JSON.stringify(secondHalf)).length);
+    const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
+    const response = await openai.embeddings.create({
+      model: "text-embedding-3-large",
+      input: firstHalf,
+    });
+    const [{ embedding: embedding1 }] = response.data;
+    const { error: error1 } = await supabase
+      .from("philosophy_data")
+      .insert({
+        work_title: title,
+        work_author: author,
+        work_content: content,
+        work_references: "Thus spoke Zarathustra by Friedrich Nietzsche.",
+        embeddings: embedding1,
+      })
+      .select("*");
+    if (error1) {
+      console.log("error", error1);
+    } else {
+      console.log("saved", `${i} ka first half`);
+    }
+    await new Promise((resolve) => setTimeout(resolve, 20000));
+    const response2 = await openai.embeddings.create({
+      model: "text-embedding-3-large",
+      input: secondHalf,
+    });
+    const [{ embedding: embedding2 }] = response2.data;
+    const { error } = await supabase
+      .from("philosophy_data")
+      .insert({
+        work_title: title,
+        work_author: author,
+        work_content: content,
+        work_references: "Beyond Good and Evil by Freidrich Nietzsche.",
+        embeddings: embedding2,
+      })
+      .select("*");
+    if (error) {
+      console.log("error", error);
+    } else {
+      console.log("saved", `${i} ka second half`);
+    }
+    return null;
+  }
+  const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
+  const response = await openai.embeddings.create({
+    model: "text-embedding-3-large",
+    input: JSON.stringify(JSONContent),
+  });
+  const [{ embedding }] = response.data;
+
+  const { error } = await supabase
+    .from("philosophy_data")
+    .insert({
+      work_title: title,
+      work_author: author,
+      work_content: content,
+      work_references: "The Gay Science by Friedrich Nietzsche.",
+      embeddings: embedding,
+    })
+    .select("*");
+  if (error) {
+    console.log("error", error);
+  } else {
+    console.log("saved", i);
+  }
+};
+
+export const createEmbeddings = async () => {
+  const loader = new PDFLoader("public/docs/nie3.pdf", {
+    parsedItemSeparator: "",
+    splitPages: false,
+  });
+  const docs = await loader.load();
+
+  const splitter = new RecursiveCharacterTextSplitter({
+    chunkSize: 25300,
+    chunkOverlap: 1500,
+  });
+
+  const output = await splitter.createDocuments([docs[0].pageContent]);
+  for (let i = 0; i < output.length; i++) {
+    await createEmbedding(
+      "The Gay Science: Nietzsche's Exploration of Joy, Truth, and Eternal Recurrence",
+      "Friedrich Nietzsche",
+      output[i].pageContent,
+      i
+    );
+     await new Promise((resolve) => setTimeout(resolve, 20000));
+  }
+};
+
+(async () => {
+  await createEmbeddings();
+})();
